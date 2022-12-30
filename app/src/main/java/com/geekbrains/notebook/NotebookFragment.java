26ece27e86b4b9594302dfe811d15bf31e495ff1
Egor Kisel <kisel1989@mail.ru>
2022-12-30 13:25:57 +0300

    + DividerItemDecoration

--- app/src/main/java/com/geekbrains/notebook/NotebookFragment.java
+++ app/src/main/java/com/geekbrains/notebook/NotebookFragment.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -45,9 +46,9 @@ public class NotebookFragment extends Fragment implements OnItemClickListener {
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(notesAdapter);
 
-//        DividerItemDecoration itemDecoration = new DividerItemDecoration(requireContext(),LinearLayoutManager.VERTICAL);
-//        itemDecoration.setDrawable(getResources().getDrawable(R.drawable.separator, null));
-//        recyclerView.addItemDecoration(itemDecoration);
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(requireContext(),LinearLayoutManager.VERTICAL);
+        itemDecoration.setDrawable(getResources().getDrawable(R.drawable.separator, null));
+        recyclerView.addItemDecoration(itemDecoration);
     }
 
     private void initAdapter() {
